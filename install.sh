fd649be43596369ea27ddbd37008500651cbb29c
Hector SB <hector.sab@hotmail.com>
2020-04-10 14:32:41 -0700

    adding nvim to init.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 echo `pwd`
 # tmux setup
 if [ ! -f ~/.tmux.conf ]; then
- ln -s "$(pwd)"/tmux/tmux.conf ~/.tmux.conf
+	ln -s "$(pwd)"/tmux/tmux.conf ~/.tmux.conf
 fi
 # Vim setup
 if [ ! -f ~/.vimrc ]; then
-  ln -s "$(pwd)"/vim/vimrc ~/.vimrc
+	ln -s "$(pwd)"/vim/vimrc ~/.vimrc
+fi
+# NeoVim setup
+if [ ! -f ~/.config/nvim/init.vim ]; then
+	if [ ! -d ~/.config/nvim ]; then
+		mkdir ~/.config/nvim
+	fi
+	ln -s "$(pwd)"/vim/vimrc ~/.config/nvim/init.vim
 fi
