03281d2e87a43faac93b404d2eb95f30d9e62347
Hector SB <hector.sab@hotmail.com>
2020-05-17 18:14:35 -0700

    Update vim with new color scheme and more
    Lots of changes. Color scheme, movements, plugins, and more.
    Inspired by some videso from  ThePrimeagen.

--- install.sh
+++ install.sh
@@ -2,15 +2,32 @@ echo `pwd`
 # tmux setup
 if [ ! -f ~/.tmux.conf ]; then
 	ln -s "$(pwd)"/tmux/tmux.conf ~/.tmux.conf
+    echo "Tmux configured"
+else
+    echo "Tmux not configured. Config file aready exists"
 fi
+
 # Vim setup
 if [ ! -f ~/.vimrc ]; then
-	ln -s "$(pwd)"/vim/vimrc ~/.vimrc
+	#ln -s "$(pwd)"/vim/init.vim ~/.vimrc
+    echo "Vim configured"
+else
+    echo "Vim not configured. Config file aready exists"
 fi
+
 # NeoVim setup
 if [ ! -f ~/.config/nvim/init.vim ]; then
 	if [ ! -d ~/.config/nvim ]; then
 		mkdir ~/.config/nvim
 	fi
-	ln -s "$(pwd)"/vim/vimrc ~/.config/nvim/init.vim
+	ln -s "$(pwd)"/vim/init.vim ~/.config/nvim/init.vim
+    echo "Nvim configured"
+else
+    echo "Nvim not configured. Config file aready exists"
 fi
+
+# Plugins
+echo "Remember to install vim-plug for the plugins!"
+echo "https://github.com/junegunn/vim-plug"
+echo "\nPlease also remember to read the notes in the (N)Vim plugins"
+echo "configurations. Some may require extra steps to make them work"
