e9736e8fca092d36470841ca684b578f731026c9
Hector SB <hector.sab@hotmail.com>
2022-03-04 03:44:33 -0600

    style(vim): tabs to spaces

--- vim/lua/user/options.lua
+++ vim/lua/user/options.lua
@@ -1,49 +1,49 @@
 local options = {
-	-- Search
-	hlsearch = true,
-	ignorecase = true,
-	smartcase = true,
-	incsearch = true,
-	--
-	backup = false,
-	swapfile = false,
-	hidden = true,
-	undofile = true,
-	writebackup = false,
-	--
-	number = true,
-	relativenumber = true,
-	numberwidth = 4,
-	--
-	splitbelow = true,
-	splitright = true,
-	-- Indentation
-	tabstop = 4,
-	softtabstop = 4,
-	shiftwidth = 4,
-	expandtab = true,
-	smartindent = true,
-	autoindent = true,
-	--
-	scrolloff = 8,
-	sidescrolloff = 8,
-	--
-	colorcolumn = {80},
-	clipboard = "unnamedplus",
-	cmdheight = 2,
-	completeopt = {"menuone", "noselect"},
-	conceallevel = 0,
-	cursorline = true,
-	fileencoding = "utf-8",
-	mouse = "",
-	pumheight = 10,
-	showmode = true,
-	showtabline = 1,
-	signcolumn = "yes",
-	termguicolors = true,
-	timeoutlen = 1000,
-	updatetime = 300,
-	wrap = false,
+  -- Search
+  hlsearch = true,
+  ignorecase = true,
+  smartcase = true,
+  incsearch = true,
+  --
+  backup = false,
+  swapfile = false,
+  hidden = true,
+  undofile = true,
+  writebackup = false,
+  --
+  number = true,
+  relativenumber = true,
+  numberwidth = 4,
+  --
+  splitbelow = true,
+  splitright = true,
+  -- Indentation
+  tabstop = 4,
+  softtabstop = 4,
+  shiftwidth = 4,
+  expandtab = true,
+  smartindent = true,
+  autoindent = true,
+  --
+  scrolloff = 8,
+  sidescrolloff = 8,
+  --
+  colorcolumn = {80},
+  clipboard = "unnamedplus",
+  cmdheight = 2,
+  completeopt = {"menuone", "noselect"},
+  conceallevel = 0,
+  cursorline = true,
+  fileencoding = "utf-8",
+  mouse = "",
+  pumheight = 10,
+  showmode = true,
+  showtabline = 1,
+  signcolumn = "yes",
+  termguicolors = true,
+  timeoutlen = 1000,
+  updatetime = 300,
+  wrap = false,
 }
 
 for k, v in pairs(options) do
@@ -54,11 +54,11 @@ end
 -- It will only show relative numbers on the window with focus or if in
 -- edit mode.
 vim.cmd([[
-	augroup numbertoggle
-		autocmd!
-		autocmd BufEnter,FocusGained,InsertLeave * set relativenumber
-		autocmd BufLeave,FocusLost,InsertEnter * set norelativenumber
-	augroup END
+  augroup numbertoggle
+    autocmd!
+    autocmd BufEnter,FocusGained,InsertLeave * set relativenumber
+    autocmd BufLeave,FocusLost,InsertEnter * set norelativenumber
+  augroup END
 ]])
 
 -- TODO: Change the colorcolumn color
