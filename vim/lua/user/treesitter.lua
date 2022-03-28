7ae361b5e2dd893e21fc84d2c5465cd79f01e752
Hector SB <hector.sab@hotmail.com>
2022-03-28 01:52:42 -0600

    fix(indentation): Disable treesitter python indent. It's broken

--- vim/lua/user/treesitter.lua
+++ vim/lua/user/treesitter.lua
@@ -12,7 +12,7 @@ configs.setup {
     disable = { "" }, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
   },
-  indent = { enable = true, disable = { "yaml" } },
+  indent = { enable = true, disable = { "yaml" , "python"} },
   rainbow = {
     enable = true,
     extended_mode = true,
